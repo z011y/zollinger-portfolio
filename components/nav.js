772de5e0acb6702a91ffa3ba602f101affa3c097
Cameron Zollinger <cameron.ntc@gmail.com>
2020-07-10 16:04:33 -0600

    fixed LogoDark styles

--- components/nav.js
+++ components/nav.js
@@ -82,6 +82,10 @@ const LogoDark = styled(Logo)`
   color: white;
   background-color: #2f80ed;
 
+  span {
+    color: white;
+  }
+
   @media (max-width: 600px) {
     font-size: 32px;
     left: 20px;
